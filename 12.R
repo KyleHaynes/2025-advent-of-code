82f0a1a17e519b9f316ff7c7f798b70adb557b2b
Kyle Haynes <5267027+KyleHaynes@users.noreply.github.com>
2025-12-13 07:28:40 +1000

    12a done

--- 12.R
+++ 12.R
@@ -8,9 +8,31 @@ options(scipen = 999)
 # Required packages.
 require(data.table)
 # Read input.
-d = data.table(v = readLines("input/03"))
+d = readLines("input/12")
+puz = d[d %plike% "x"]
+puz = rbindlist(lapply(strsplit(puz, " "), function(x) data.table(t(x))))
+te = d[!d %plike% "x"]
+te = list(
+    "1" = te[2:4],
+    "2" = te[7:9],
+    "3" = te[12:14],
+    "4" = te[17:19],
+    "5" = te[22:24],
+    "6" = te[27:29]
+)
+te = unlist(lapply(te, function(x) sum(strsplit(paste(x, collapse = ""), "")[[1]] == "#")), use.names = F)
 
 ## Part 1.
-
+puz[, num_hash := {
+    (as.numeric(V2) * te[1]) + 
+    (as.numeric(V3) * te[2]) + 
+    (as.numeric(V4) * te[3]) + 
+    (as.numeric(V5) * te[4]) + 
+    (as.numeric(V6) * te[5]) +
+    (as.numeric(V7) * te[6])
+}]
+puz[, sr := as.numeric(gsub("x.*", "", V1)) * as.numeric(gsub(".*x|:", "", V1))]
+puz[!sr < num_hash, .N]
+# 538
 
 ## Part 2.
